6fddb622f628e8e9d164ac2b1e2c057eadf5447b
Andy Joshowitz <ajoshow@sas.upenn.edu>
2018-05-15 21:46:40 -0400

    course/instructor models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,43 @@
 class Course < ApplicationRecord
+  has_many :reviews
+  belongs_to :user
+
+  belongs_to :user
+has_many :activity_categories
+has_many :categories, through: :activity_categories
+has_many :reviews
+has_many :photos
+belongs_to :location, optional: true
+
+validates_presence_of :title
+validates :title, length: { maximum: 60 }
+validates_presence_of :description
+
+  def instructor_attributes=(instructor_attributes)
+    self.build_instructor(instructor_attributes)
+  end
+
+  def self.most_popular
+    joins(:reviews).group("courses.id").order("(sum(reviews.course_quality)/count(*)) desc")
+  end
+
+  def avg_difficulty
+    if !reviews.empty?
+      self.reviews.collect{|r| r.difficulty}.inject(0){|sum,x| sum + x }/self.reviews.count.to_f
+    else
+      "No reviews"
+    end
+  end
+
+  def self.no_reviews
+    Activity.includes(:reviews).where( :reviews => { :id => nil } )
+  end
+
+  def self.first_ten
+    limit(10)
+  end
+
+  def self.recently_added
+    order(created_at: :desc)
+  end
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,2 +1,22 @@
 class Instructor < ApplicationRecord
+  has_many :courses
+  validates_presence_of :first_name, :last_name
+
+  def self.most_popular_instructors
+    joins(:courses).group(:id).order('count(courses.id) DESC')
+  end
+
+
+  def courses_attributes=(courses_attributes)
+    courses_attributes.each do |i, courses_attributes|
+      if !course_attributes[:title].blank? && !course_attributes[:description].blank?
+        if course_attributes[:id].blank?
+          self.courses.build(course_attributes)
+        else
+          course = Course.find_by(id: course_attributes[:id])
+          course.update(course_attributes)
+        end
+      end
+    end
+  end
 end
