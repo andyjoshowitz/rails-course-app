a5ed5aa087ee699bdebb2f797344f88c9a35e53b
Andy Joshowitz <ajoshow@sas.upenn.edu>
2018-05-15 21:47:59 -0400

    review/user models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,8 @@
 class Review < ApplicationRecord
+  belongs_to :user
+  belongs_to :course
+
+  validates_presence_of :difficulty, :course_quality, :instructor_quality, :amount_learned, :work_amount
+  validates :difficulty, :course_quality, :instructor_quality, :amount_learned, :work_amount, numericality: { only_integer: true }
+  validates :difficulty, :course_quality, :instructor_quality, :amount_learned, :work_amount, inclusion: 1..5
 end
